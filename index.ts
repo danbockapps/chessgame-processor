338cd98c5ebcbe07a3fed1ce00aea26490c7c857
Dan Bock <danbock@gmail.com>
2022-01-24 12:58:49 -0500

    Read in pgn and analyze all moves

---
+++ index.ts
@@ -0,0 +1,38 @@
+import { Chess } from 'chess.js'
+import { spawn } from 'child_process'
+import fs from 'fs'
+
+const file = fs.readFileSync('pgn.pgn', 'utf-8')
+const games = file.split(/\s(?=\[Event .*\])/)
+
+const ch = new Chess()
+
+const fens = games.flatMap(game => {
+  ch.load_pgn(game)
+  const ch2 = new Chess()
+  return ch.history().map(move => {
+    ch2.move(move)
+    return { move, fen: ch2.fen() }
+  })
+})
+
+let i = 0
+
+const nextMove = () => {
+  if (i >= fens.length) process.exit()
+  console.log('Now evaluating ' + fens[i].move)
+  console.time('Evaluation time')
+  st.stdin.write(`position fen ${fens[i].fen}\ngo wtime 10000 btime 10000\n`)
+  i++
+}
+
+const st = spawn('stockfish')
+st.stdout.on('data', chunk => {
+  const output: string = chunk.toString('utf-8')
+  const split = output.split('bestmove')
+  if (output.includes('Stockfish') || split[1]) {
+    console.timeEnd('Evaluation time')
+    console.log('Best move:', split[1])
+    nextMove()
+  }
+})
